328be2f78ab5bfb306c7216ef491a625908be90c
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-12 02:16:00 +0900

    feat(attr): implement almost Attr API

--- nodes/attr.ts
+++ nodes/attr.ts
@@ -4,109 +4,198 @@ import { Document } from "./document.ts";
 import { type Element } from "./element.ts";
 import type { IAttr } from "../interface.d.ts";
 
+export const $namespace = Symbol();
+export const $namespacePrefix = Symbol();
+export const $element = Symbol();
+export const $value = Symbol();
+export const $localName = Symbol();
+
 export class Attr extends Node implements IAttr {
-  #namespace: string | null;
-  #namespacePrefix: string | null;
-  #localName: string;
-  #value: string;
-  #attributeElement: null | Element;
+  [$namespace]: string | null;
+  [$namespacePrefix]: string | null;
+  [$localName]: string;
+  [$value]: string = "";
+  [$element]: Element | null;
 
   constructor(localName: string) {
     super();
 
-    this.#namespace = null;
-    this.#namespacePrefix = null;
-    this.#attributeElement = null;
-    this.#value = "";
-    this.#localName = localName;
-    this.name = "";
+    this[$namespace] = null;
+    this[$namespacePrefix] = null;
+    this[$element] = null;
+    this[$value] = "";
+    this[$localName] = localName;
   }
 
-  override nodeDocument: Document = new Document();
-
-  override get nodeName(): string {
-    const prefix = this.#namespacePrefix;
+  /**
+   * @see https://dom.spec.whatwg.org/#concept-attribute-qualified-name
+   */
+  get #qualifiedName(): string {
+    // An attribute’s qualified name is its local name if its namespace prefix is null, and its namespace prefix, followed by ":", followed by its local name, otherwise.
+    const prefix = this[$namespacePrefix];
     const qualifiedName = prefix === null
-      ? this.#localName
-      : `${prefix}:${this.#localName}`;
+      ? this[$localName]
+      : `${prefix}:${this[$localName]}`;
 
     return qualifiedName;
   }
 
+  override nodeDocument: Document = new Document();
+
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-node-nodetype
+   */
+  override get nodeType(): NodeType.ATTRIBUTE_NODE {
+    return NodeType.ATTRIBUTE_NODE;
+  }
+
+  override get nodeName(): string {
+    return this.#qualifiedName;
+  }
+
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-node-nodevalue
+   */
   override get nodeValue(): string {
-    return this.#value;
+    // this’s value.
+    return this[$value];
   }
 
-  override set nodeValue(value: string | null) {
-    throw new UnImplemented();
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-node-nodevalue
+   */
+  override set nodeValue(value: string) {
+    // Set an existing attribute value with this and the given value.
+    setAnExistingAttributeValue(this, value);
   }
 
-  override get textContent(): string | null {
-    throw new UnImplemented();
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-node-textcontent
+   */
+  override get textContent(): string {
+    // this’s value.
+    return this[$value];
   }
-  override set textContent(value: string | null) {
-    throw new UnImplemented();
+
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-node-textcontent
+   */
+  override set textContent(value: string) {
+    // Set an existing attribute value with this and the given value.
+    setAnExistingAttributeValue(this, value);
   }
 
   override isEqualNode(otherNode: Node | null): boolean {
     throw new UnImplemented();
   }
 
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-namespaceuri
+   */
   get namespaceURI(): string | null {
-    return this.#namespace;
+    // The namespaceURI getter steps are to return this’s namespace.
+    return this[$namespace];
   }
 
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-prefix
+   */
   get prefix(): string | null {
-    return this.#namespacePrefix;
+    // The prefix getter steps are to return this’s namespace prefix.
+    return this[$namespacePrefix];
   }
 
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-localname
+   */
   get localName(): string {
-    return this.#localName;
+    // The localName getter steps are to return this’s local name.
+    return this[$localName];
   }
 
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-name
+   */
+  get name(): string {
+    // The name getter steps are to return this’s qualified name.
+    return this.#qualifiedName;
+  }
+
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-value
+   */
   get value(): string {
-    return this.#value;
+    // The value getter steps are to return this’s value.
+    return this[$value];
   }
 
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-value
+   */
   set value(value: string) {
+    // The value getter steps are to return this’s value.
     setAnExistingAttributeValue(this, value);
   }
 
-  get ownerElement(): any | null {
-    return this.#attributeElement;
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-ownerelement
+   */
+  get ownerElement(): Element | null {
+    // The ownerElement getter steps are to return this’s element.
+    return this[$element];
   }
 
-  get specified(): boolean {
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-attr-specified
+   */
+  get specified(): true {
+    // The specified getter steps are to return true.
     return true;
   }
-
-  name: string;
 }
 
-function appendAttributes(attribute: Attr, element: Element) {
-  element.attributes.setNamedItem(attribute);
-  attribute.ownerElement = element;
-}
-
-export function setAnExistingAttributeValue(attribute: Attr, value: string) {
-  if (attribute.ownerElement === null) {
-    attribute.value = value;
+/**
+ * @see https://dom.spec.whatwg.org/#set-an-existing-attribute-value
+ */
+export function setAnExistingAttributeValue(
+  attribute: Attr,
+  value: string,
+): void {
+  // 1. If attribute’s element is null, then set attribute’s value to value.
+  if (attribute[$element] === null) {
+    attribute[$value] = value;
   } else {
+    // 2. Otherwise, change attribute to value.
     changeAttributes(attribute, value);
   }
 }
 
+/**
+ * @see https://dom.spec.whatwg.org/#concept-element-attributes-change
+ */
 export function changeAttributes(attribute: Attr, value: string): void {
-  const oldValue = attribute.value;
-  attribute.value = value;
+  // 1. Let oldValue be attribute’s value.
+  const oldValue = attribute[$value];
 
-  handleAttributesChanges(attribute, attribute.ownerElement, oldValue, value);
+  // 2. Set attribute’s value to value.
+  attribute[$value] = value;
+
+  // 3. Handle attribute changes for attribute with attribute’s element, oldValue, and value.
+  handleAttributesChanges(attribute, attribute[$element], oldValue, value);
 }
 
+/**
+ * @see https://dom.spec.whatwg.org/#handle-attribute-changes
+ */
 export function handleAttributesChanges(
   attribute: Attr,
-  element: Element,
-  oldValue: Attr,
-  newValue: Attr,
+  element: Element | null,
+  oldValue: string,
+  newValue: string,
 ): void {
+  // 1. Queue a mutation record of "attributes" for element with attribute’s local name, attribute’s namespace, oldValue, « », « », null, and null.
+
+  // 2. If element is custom, then enqueue a custom element callback reaction with element, callback name "attributeChangedCallback", and an argument list containing attribute’s local name, oldValue, newValue, and attribute’s namespace.
+
+  // 3. Run the attribute change steps with element, attribute’s local name, oldValue, newValue, and attribute’s namespace.
 }
