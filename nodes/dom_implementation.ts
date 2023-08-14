bb44b869145098ea2e1514f6ee72f44d8696d9cc
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-14 12:04:17 +0900

    feat(dom_implementation): implement DOMImplementation#hasFeature

--- nodes/dom_implementation.ts
+++ nodes/dom_implementation.ts
@@ -1,3 +1,32 @@
+import { UnImplemented } from "./utils.ts";
 import type { IDOMImplementation } from "../interface.d.ts";
 
-export class DOMImplementation implements IDOMImplementation {}
+export class DOMImplementation implements IDOMImplementation {
+  createDocumentType(
+    qualifiedName: string,
+    publicId: string,
+    systemId: string,
+  ): DocumentType {
+    throw new UnImplemented();
+  }
+
+  createDocument(
+    namespace: string | null,
+    qualifiedName: string | null,
+    doctype?: DocumentType | null | undefined,
+  ): XMLDocument {
+    throw new UnImplemented();
+  }
+
+  createHTMLDocument(title?: string | undefined): Document {
+    throw new UnImplemented();
+  }
+
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-domimplementation-hasfeature
+   */
+  hasFeature(..._: readonly unknown[]): true {
+    // return true
+    return true;
+  }
+}
