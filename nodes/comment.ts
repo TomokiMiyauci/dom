0a74340cac1f4043428b0fed8068b9d5d3d51886
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-14 17:11:55 +0900

    feat(comment): implement Comment constructor

--- nodes/comment.ts
+++ nodes/comment.ts
@@ -5,6 +5,14 @@ import { UnImplemented } from "./utils.ts";
 import { IComment } from "../interface.d.ts";
 
 export class Comment extends CharacterData implements IComment {
+  /**
+   * @see https://dom.spec.whatwg.org/#dom-comment-comment
+   */
+  constructor(data: string = "") {
+    // set this’s data to data and this’s node document to current global object’s associated Document.
+    super(data, globalThis.document);
+  }
+
   override get nodeType(): NodeType.COMMENT_NODE {
     return NodeType.COMMENT_NODE;
   }
