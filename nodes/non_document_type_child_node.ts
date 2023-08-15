43ec54e83c12915599c41893e4bbd098cd36e4ad
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-15 16:54:42 +0900

    chore(non_document_type_child_node): add NonDocumentTypeChildNode mixin mock

--- nodes/non_document_type_child_node.ts
+++ nodes/non_document_type_child_node.ts
@@ -1,6 +1,27 @@
 import { type INonDocumentTypeChildNode } from "../interface.d.ts";
+import { UnImplemented } from "./utils.ts";
+import type { Element } from "./element.ts";
+import { type Constructor } from "../deps.ts";
 
-export class NonDocumentTypeChildNode implements INonDocumentTypeChildNode {
-  nextElementSibling: any | null;
-  previousElementSibling: any | null;
+export function NonDocumentTypeChildNode<T extends Constructor>(Ctor: T) {
+  return class extends Ctor implements INonDocumentTypeChildNode {
+    get nextElementSibling(): Element | null {
+      throw new UnImplemented();
+    }
+
+    set nextElementSibling(value: Element | null) {
+      throw new UnImplemented();
+    }
+
+    get previousElementSibling(): Element | null {
+      throw new UnImplemented();
+    }
+
+    set previousElementSibling(value: Element | null) {
+      throw new UnImplemented();
+    }
+  };
 }
+
+// deno-lint-ignore no-empty-interface
+export interface NonDocumentTypeChildNode extends INonDocumentTypeChildNode {}
