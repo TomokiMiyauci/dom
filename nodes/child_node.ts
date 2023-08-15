7d4555069168ff334eae4932c5e905fd7075008b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-15 16:49:09 +0900

    chore(child_node): add ChildNode mixin mock

--- nodes/child_node.ts
+++ nodes/child_node.ts
@@ -1,21 +1,27 @@
 import { type Node } from "./node.ts";
 import { UnImplemented } from "./utils.ts";
 import type { IChildNode } from "../interface.d.ts";
+import { Constructor } from "../deps.ts";
 
-export class ChildNode implements IChildNode {
-  after(...nodes: (string | Node)[]): void {
-    throw new UnImplemented();
-  }
+export function ChildNode<T extends Constructor>(Ctor: T) {
+  return class extends Ctor implements ChildNode {
+    after(...nodes: (string | Node)[]): void {
+      throw new UnImplemented();
+    }
 
-  before(...nodes: (string | Node)[]): void {
-    throw new UnImplemented();
-  }
+    before(...nodes: (string | Node)[]): void {
+      throw new UnImplemented();
+    }
 
-  replaceWith(...nodes: (string | Node)[]): void {
-    throw new UnImplemented();
-  }
+    replaceWith(...nodes: (string | Node)[]): void {
+      throw new UnImplemented();
+    }
 
-  remove(): void {
-    throw new UnImplemented();
-  }
+    remove(): void {
+      throw new UnImplemented();
+    }
+  };
 }
+
+// deno-lint-ignore no-empty-interface
+export interface ChildNode extends IChildNode {}
