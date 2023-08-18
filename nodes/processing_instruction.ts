a6547423c7647e613024d2cf79e865da6361ddf9
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-08-18 17:05:40 +0900

    chore(processing_instruction): add ProcessingInstruction

---
+++ nodes/processing_instruction.ts
@@ -0,0 +1,52 @@
+import type { IProcessingInstruction } from "../interface.d.ts";
+import { CharacterData, CharacterDataStates } from "./character_data.ts";
+import { LinkStyle } from "../cssom/link_style.ts";
+import { NodeStates, NodeType } from "./node.ts";
+import { Document } from "./document.ts";
+import { $data } from "./internal.ts";
+
+export interface ProcessingInstructionInits {
+  target: string;
+}
+
+@LinkStyle
+export class ProcessingInstruction extends CharacterData
+  implements IProcessingInstruction {
+  #target: string;
+
+  constructor(
+    { data, nodeDocument, target }:
+      & ProcessingInstructionInits
+      & CharacterDataStates
+      & NodeStates,
+  ) {
+    super(data, nodeDocument);
+    this.#target = target;
+  }
+
+  override readonly nodeType: NodeType.PROCESSING_INSTRUCTION_NODE =
+    NodeType.PROCESSING_INSTRUCTION_NODE;
+
+  override get nodeName(): string {
+    return this.#target;
+  }
+
+  get target(): string {
+    return this.#target;
+  }
+
+  protected override equals(other: this): boolean {
+    return this.#target === other.#target && this[$data] === other[$data];
+  }
+
+  protected override clone(document: Document): ProcessingInstruction {
+    return new ProcessingInstruction({
+      data: this[$data],
+      target: this.#target,
+      nodeDocument: document,
+    });
+  }
+}
+
+// deno-lint-ignore no-empty-interface
+export interface ProcessingInstruction extends LinkStyle {}
