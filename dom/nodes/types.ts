7af44e2f9a78f06a5ef4d2cff3febec8a3b45e28
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-09-15 16:55:57 +0900

    refactor(dom): remove unused

--- dom/nodes/types.ts
+++
@@ -1,6 +0,0 @@
-import { type Node } from "./node.ts";
-import type { ChildNode } from "./node_trees/child_node.ts";
-import type { ParentNode } from "./node_trees/parent_node.ts";
-
-export type Child = Node & ChildNode;
-export type Parent = Node & ParentNode;
