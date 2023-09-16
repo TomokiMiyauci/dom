e308843b7ac2c7849c711471c6be85bcb7e97bce
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-09-16 10:40:30 +0900

    refactor(range): split algorithm

--- dom/ranges/range.ts
+++ dom/ranges/range.ts
@@ -3,34 +3,24 @@ import { Exposed } from "../../webidl/extended_attribute.ts";
 import { Const, constant } from "../../webidl/idl.ts";
 import { AbstractRange, AbstractRangeInternals } from "./abstract_range.ts";
 import { BoundaryPoint, Position, position } from "./boundary_point.ts";
-import {
-  isCharacterData,
-  isComment,
-  isDocumentFragment,
-  isDocumentType,
-  isProcessingInstruction,
-  isText,
-} from "../nodes/utils.ts";
+import { isDocumentType, isText } from "../nodes/utils.ts";
 import { DOMExceptionName } from "../../webidl/exception.ts";
-import { DocumentFragment } from "../nodes/document_fragment.ts";
 import { nodeLength } from "../nodes/node_trees/node_tree.ts";
-import { substringData } from "../nodes/character_data.ts";
-import {
-  appendNode,
-  ensurePreInsertionValidity,
-  preInsertNode,
-  removeNode,
-} from "../nodes/node_trees/mutation.ts";
-import { replaceData } from "../nodes/character_data_algorithm.ts";
 import {
   substringCodeUnitByPositions,
   substringCodeUnitToEnd,
 } from "../../infra/string.ts";
 import { convert, unsignedLong, unsignedShort } from "../../webidl/types.ts";
-import { splitText } from "../nodes/text.ts";
 import { Range_CSSOM } from "../../cssom/range.ts";
 import { Range_DOMParsing } from "../../domparsing/range.ts";
 import { $, internalSlots, tree } from "../../internal.ts";
+import {
+  cloneContents,
+  extract,
+  insert,
+  nextNodeDescendant,
+  select,
+} from "./range_utils.ts";
 
 @Range_CSSOM
 @Range_DOMParsing
@@ -403,10 +393,7 @@ export class Range extends AbstractRange implements IRange {
     // 1. Let s be the empty string.
     let s = "";
 
-    const startNode = this._.startNode,
-      endNode = this._.endNode,
-      startOffset = this._.startOffset,
-      endOffset = this._.endOffset;
+    const { startNode, endNode, startOffset, endOffset } = this._;
 
     // 2. If this’s start node is this’s end node and it is a Text node, then return the substring of that Text node’s data beginning at this’s start offset and ending at this’s end offset.
     if (startNode === endNode && isText(startNode)) {
@@ -514,16 +501,6 @@ export class Range extends AbstractRange implements IRange {
   }
 }
 
-function nextNodeDescendant(node: Node | null): Node | null {
-  while (node && tree.nextSibling(node)) {
-    node = tree.parent(node);
-  }
-
-  if (!node) return null;
-
-  return tree.nextSibling(node);
-}
-
 export interface Range
   extends
     Const<"START_TO_START", 0>,
@@ -532,313 +509,3 @@ export interface Range
     Const<"END_TO_START", 3>,
     Range_CSSOM,
     Range_DOMParsing {}
-
-function cloneContents(range: Range): globalThis.DocumentFragment {
-  // 1. Let fragment be a new DocumentFragment node whose node document is range’s start node’s node document.
-  const fragment = DocumentFragment["create"]({
-    nodeDocument: $($(range).startNode).nodeDocument,
-  });
-
-  // 2. If range is collapsed, then return fragment.
-  if ($(range).collapsed) return fragment;
-
-  // 3. Let original start node, original start offset, original end node, and original end offset be range’s start node, start offset, end node, and end offset, respectively.
-  const originalStartNode = $(range).startNode,
-    originalStartOffset = $(range).startOffset,
-    originalEndNode = $(range).endNode,
-    originalEndOffset = $(range).endOffset;
-
-  // 4. If original start node is original end node and it is a CharacterData node, then:
-  if (
-    originalStartNode === originalEndNode && isCharacterData(originalStartNode)
-  ) {
-    // 1. Let clone be a clone of original start node.
-    const clone = originalStartNode.cloneNode() as CharacterData;
-
-    // 2. Set the data of clone to the result of substringing data with node original start node, offset original start offset, and count original end offset minus original start offset.
-    $(clone).data = substringData(
-      originalStartNode,
-      originalStartOffset,
-      originalEndOffset - originalStartOffset,
-    );
-
-    // 3. Append clone to fragment.
-    appendNode(clone, fragment);
-
-    // 4. Return fragment.
-    return fragment;
-  }
-
-  // 5. Let common ancestor be original start node.
-  let commonAncestor = originalStartNode;
-
-  // 6. While common ancestor is not an inclusive ancestor of original end node, set common ancestor to its own parent.
-  while (!tree.isInclusiveAncestor(commonAncestor, originalEndNode)) {
-    const parent = tree.parent(commonAncestor);
-    if (!parent) break;
-
-    commonAncestor = parent;
-  }
-
-  // 7. Let first partially contained child be null.
-  let firstPartiallyContainedChild = null;
-
-  // 8. If original start node is not an inclusive ancestor of original end node, set first partially contained child to the first child of common ancestor that is partially contained in range.
-  if (!tree.isInclusiveAncestor(originalStartNode, originalEndNode)) {
-    firstPartiallyContainedChild;
-  }
-
-  // 9. Let last partially contained child be null.
-  let lastPartiallyContainedChild: Node | null = null;
-
-  // 10. If original end node is not an inclusive ancestor of original start node, set last partially contained child to the last child of common ancestor that is partially contained in range.
-  // 11. Let contained children be a list of all children of common ancestor that are contained in range, in tree order.
-  // 12. If any member of contained children is a doctype, then throw a "HierarchyRequestError" DOMException.
-
-  // 13. If first partially contained child is a CharacterData node, then:
-  if (
-    firstPartiallyContainedChild &&
-    isCharacterData(firstPartiallyContainedChild)
-  ) {
-    // 1. Let clone be a clone of original start node.
-    // 2. Set the data of clone to the result of substringing data with node original start node, offset original start offset, and count original start node’s length − original start offset.
-    // 3. Append clone to fragment.
-
-    // 14. Otherwise, if first partially contained child is not null:
-  } else if (firstPartiallyContainedChild) {
-    // 1. Let clone be a clone of first partially contained child.
-    // 2. Append clone to fragment.
-    // 3. Let subrange be a new live range whose start is (original start node, original start offset) and whose end is (first partially contained child, first partially contained child’s length).
-    // 4. Let subfragment be the result of cloning the contents of subrange.
-    // 5. Append subfragment to clone.
-  }
-
-  // 15. For each contained child in contained children:
-  // 1. Let clone be a clone of contained child with the clone children flag set.
-  // 2. Append clone to fragment.
-
-  // 16. If last partially contained child is a CharacterData node, then:
-  if (
-    lastPartiallyContainedChild && isCharacterData(lastPartiallyContainedChild)
-  ) {
-    // 1. Let clone be a clone of original end node.
-    // 2. Set the data of clone to the result of substringing data with node original end node, offset 0, and count original end offset.
-    // 3. Append clone to fragment.
-
-    // 17. Otherwise, if last partially contained child is not null:
-  } else if (lastPartiallyContainedChild) {
-    // 1. Let clone be a clone of last partially contained child.
-
-    // 2. Append clone to fragment.
-    // 3. Let subrange be a new live range whose start is (last partially contained child, 0) and whose end is (original end node, original end offset).
-    // 4. Let subfragment be the result of cloning the contents of subrange.
-    // 5. Append subfragment to clone.
-  }
-
-  // 18. Return fragment.
-  return fragment;
-}
-
-/**
- * @see https://dom.spec.whatwg.org/#concept-range-extract
- */
-function extract(range: Range): globalThis.DocumentFragment {
-  // 1. Let fragment be a new DocumentFragment node whose node document is range’s start node’s node document.
-  const fragment = DocumentFragment["create"]({
-    nodeDocument: $($(range).startNode).nodeDocument,
-  });
-
-  // 2. If range is collapsed, then return fragment.
-  if ($(range).collapsed) return fragment;
-
-  // 3. Let original start node, original start offset, original end node, and original end offset be range’s start node, start offset, end node, and end offset, respectively.
-  const {
-    startNode: originalStartNode,
-    startOffset: originalStartOffset,
-    endNode: originalEndNode,
-    endOffset: originalEndOffset,
-  } = $(range);
-
-  // 4. If original start node is original end node and it is a CharacterData node, then:
-  if (
-    originalStartNode === originalEndNode &&
-    isCharacterData(originalStartNode)
-  ) {
-    // 1. Let clone be a clone of original start node.
-    // TODO(miyauci): use internal method
-    const clone = originalStartNode.cloneNode() as CharacterData;
-
-    // 2. Set the data of clone to the result of substringing data with node original start node, offset original start offset, and count original end offset minus original start offset.
-    const data = substringData(
-      originalStartNode,
-      originalStartOffset,
-      originalEndOffset - originalStartOffset,
-    );
-    $(clone).data = data;
-
-    // 3. Append clone to fragment.
-    appendNode(clone, fragment);
-
-    // 4. Replace data with node original start node, offset original start offset, count original end offset minus original start offset, and data the empty string.
-    replaceData(
-      originalStartNode,
-      originalStartOffset,
-      originalEndOffset - originalStartOffset,
-      "",
-    );
-
-    // 5. Return fragment.
-    return fragment;
-  }
-
-  // 5. Let common ancestor be original start node.
-  let commonAncestor = originalStartNode;
-
-  // 6. While common ancestor is not an inclusive ancestor of original end node, set common ancestor to its own parent.
-  while (tree.isInclusiveAncestor(commonAncestor, originalEndNode)) {
-    const parent = tree.parent(commonAncestor);
-
-    if (!parent) break;
-
-    commonAncestor = parent;
-  }
-
-  // 7. Let first partially contained child be null.
-  let firstPartiallyContainedChild = null;
-
-  // 8. If original start node is not an inclusive ancestor of original end node, set first partially contained child to the first child of common ancestor that is partially contained in range.
-  if (tree.isInclusiveAncestor(originalStartNode, originalEndNode)) {}
-
-  // 9. Let last partially contained child be null.
-  let lastPartiallyContainedChild = null;
-
-  // 10. If original end node is not an inclusive ancestor of original start node, set last partially contained child to the last child of common ancestor that is partially contained in range.
-
-  // 11. Let contained children be a list of all children of common ancestor that are contained in range, in tree order.
-
-  // 12. If any member of contained children is a doctype, then throw a "HierarchyRequestError" DOMException.
-
-  // 13. If original start node is an inclusive ancestor of original end node, set new node to original start node and new offset to original start offset.
-
-  // 14. Otherwise:
-  // 1. Let reference node equal original start node.
-  // 2. While reference node’s parent is not null and is not an inclusive ancestor of original end node, set reference node to its parent.
-  // 3. Set new node to the parent of reference node, and new offset to one plus reference node’s index.
-
-  // 15. If first partially contained child is a CharacterData node, then:
-  if (isCharacterData(firstPartiallyContainedChild as any)) {
-    // 1. Let clone be a clone of original start node.
-
-    // 2. Set the data of clone to the result of substringing data with node original start node, offset original start offset, and count original start node’s length − original start offset.
-
-    // 3. Append clone to fragment.
-
-    // 4. Replace data with node original start node, offset original start offset, count original start node’s length − original start offset, and data the empty string.
-  }
-
-  // 16. Otherwise, if first partially contained child is not null:
-  // 1. Let clone be a clone of first partially contained child.
-  // 2. Append clone to fragment.
-  // 3. Let subrange be a new live range whose start is (original start node, original start offset) and whose end is (first partially contained child, first partially contained child’s length).
-  // 4. Let subfragment be the result of extracting subrange.
-  // 5. Append subfragment to clone.
-
-  // 17. For each contained child in contained children, append contained child to fragment.
-  // 18. If last partially contained child is a CharacterData node, then:
-
-  // 1. Let clone be a clone of original end node.
-  // 2. Set the data of clone to the result of substringing data with node original end node, offset 0, and count original end offset.
-  // 3. Append clone to fragment.
-  // 4. Replace data with node original end node, offset 0, count original end offset, and data the empty string.
-  // 19. Otherwise, if last partially contained child is not null:
-  // 1. Let clone be a clone of last partially contained child.
-  // 2. Append clone to fragment.
-  // 3. Let subrange be a new live range whose start is (last partially contained child, 0) and whose end is (original end node, original end offset).
-  // 4. Let subfragment be the result of extracting subrange.
-  // 5. Append subfragment to clone.
-
-  // 20. Set range’s start and end to (new node, new offset).
-  // 21. Return fragment.
-  return fragment;
-}
-
-/**
- * @see [DOM Living Standard](https://dom.spec.whatwg.org/#concept-range-insert)
- */
-function insert(node: Node, range: Range): void {
-  const { startNode } = $(range);
-
-  // 1. If range’s start node is a ProcessingInstruction or Comment node, is a Text node whose parent is null, or is node, then throw a "HierarchyRequestError" DOMException.
-  if (
-    isProcessingInstruction(startNode) || isComment(startNode) ||
-    (isText(startNode) && !tree.parent(startNode)) ||
-    startNode === node
-  ) throw new DOMException("<message>", DOMExceptionName.HierarchyRequestError);
-
-  // 2. Let referenceNode be null.
-  let referenceNode: Node | null = null;
-
-  // 3. If range’s start node is a Text node, set referenceNode to that Text node.
-  if (isText(startNode)) referenceNode = startNode;
-  // 4. Otherwise, set referenceNode to the child of start node whose index is start offset, and null if there is no such child.
-  else {referenceNode = tree.children(startNode)[$(range).startOffset] ??
-      null;}
-
-  // 5. Let parent be range’s start node if referenceNode is null, and referenceNode’s parent otherwise.
-  const parent = !referenceNode ? startNode : tree.parent(referenceNode);
-
-  if (!parent) return;
-
-  // 6. Ensure pre-insertion validity of node into parent before referenceNode.
-  ensurePreInsertionValidity(node, parent, referenceNode);
-
-  // 7. If range’s start node is a Text node, set referenceNode to the result of splitting it with offset range’s start offset.
-  if (isText(startNode)) {
-    referenceNode = splitText(startNode, $(range).startOffset);
-  }
-
-  // 8. If node is referenceNode, set referenceNode to its next sibling.
-  if (node === referenceNode) referenceNode = tree.nextSibling(node);
-
-  // 9. If node’s parent is non-null, then remove node.
-  if (tree.parent(node)) removeNode(node);
-
-  // 10. Let newOffset be parent’s length if referenceNode is null; otherwise referenceNode’s index.
-  let newOffset = !referenceNode
-    ? nodeLength(parent)
-    : tree.index(referenceNode);
-
-  // 11. Increase newOffset by node’s length if node is a DocumentFragment node; otherwise 1.
-  newOffset += isDocumentFragment(node) ? nodeLength(node) : 1;
-
-  // 12. Pre-insert node into parent before referenceNode.
-  preInsertNode(node, parent, referenceNode);
-
-  // 13. If range is collapsed, then set range’s end to (parent, newOffset).
-  if ($(range).collapsed) $(range).end = [parent, newOffset];
-}
-
-/**
- * @see https://dom.spec.whatwg.org/#concept-range-select
- */
-function select(node: Node, range: Range): void {
-  // 1. Let parent be node’s parent.
-  const parent = tree.parent(node);
-
-  // 2. If parent is null, then throw an "InvalidNodeTypeError" DOMException.
-  if (!parent) {
-    throw new DOMException(
-      "<message>",
-      DOMExceptionName.InvalidNodeTypeError,
-    );
-  }
-
-  // 3. Let index be node’s index.
-  const index = tree.index(node);
-
-  // 4. Set range’s start to boundary point (parent, index).
-  $(range).start = [parent, index];
-
-  // 5. Set range’s end to boundary point (parent, index plus 1).
-  $(range).end = [parent, index + 1];
-}
